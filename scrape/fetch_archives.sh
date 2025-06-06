21868ed989389ad47157cf58d3ebbe236b94291e
Jeff Mulligan <jbmull@gmail.com>
2025-06-06 08:03:40 -0700

    issue-3: now fetches all the archive files

--- scrape/fetch_archives.sh
+++ scrape/fetch_archives.sh
@@ -8,7 +8,49 @@
 #	-d "adminpw=ZLxq5BULVCvi"			\
 #	http://nephoscale.ewind.com/mailman/admin/cvnet
 
-curl	-b cookies.txt					\
-	http://nephoscale.ewind.com/mailman/private/cvnet/2025-May.txt.gz \
-	> archive_data/2025-May.txt 2>> curlerrs
+if [[ -e curlerrs ]]; then
+  echo Removing old error file...
+  rm curlerrs
+fi
+
+fetch_one_month(){
+	m=$1
+	y=$2
+	echo Fetching $m $y ...
+	src=http://nephoscale.ewind.com/mailman/private/cvnet/$y-$m.txt.gz
+	curl	-b cookies.txt	$src			\
+		> archive_data/$y-$m.txt 2>> curlerrs
+}
+
+for year in 2018 2020 2021 2022 2023 2024
+do
+  for month in January February March April May June July August September October November December
+  do
+    fetch_one_month $month $year
+  done
+done
+
+for year in 2019
+do
+  for month in January February March April May June October November December
+  do
+    fetch_one_month $month $year
+  done
+done
+
+for year in 2017
+do
+  for month in October November December
+  do
+    fetch_one_month $month $year
+  done
+done
+
+for year in 2025
+do
+  for month in January February March April May
+  do
+    fetch_one_month $month $year
+  done
+done
 
